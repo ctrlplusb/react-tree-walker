5677d18b7c3353fc539bf5eddb7ffa204f8f14b8
Sean Matheson <sean@ctrlplusb.com>
2018-03-16 17:26:29 +0000

    Errors are no longer caught/swallowed internally, leaving error handling to the calling code.

--- src/index.js
+++ src/index.js
@@ -60,157 +60,171 @@ export const isPromise = x => x != null && typeof x.then === 'function'
 // If visitor returns `false`, don't call the element's render function
 // or recurse into its child elements
 export default function reactTreeWalker(
-  element,
+  rootElement,
   visitor,
-  context,
+  rootContext,
   options = defaultOptions,
 ) {
-  return new Promise(resolve => {
-    const doVisit = (getChildren, visitResult, childContext) => {
-      const visitChildren = () => {
-        const child = ensureChild(getChildren())
-        const theChildContext =
-          typeof childContext === 'function' ? childContext() : childContext
-
-        if (child == null) {
-          // If no children then we can't traverse.  We've reached the leaf.
-          resolve()
-        } else if (Children.count(child)) {
-          // If its a react Children collection we need to breadth-first
-          // traverse each of them.
-          const mapper = aChild =>
-            aChild
-              ? reactTreeWalker(aChild, visitor, theChildContext, options)
-              : undefined
-          // pMapSeries allows us to do depth-first traversal. Thanks @sindresorhus!
-          pMapSeries(Children.map(child, cur => cur), mapper).then(resolve)
-        } else {
-          // Otherwise we pass the individual child to the next recursion.
-          reactTreeWalker(child, visitor, theChildContext, options).then(
-            resolve,
-          )
+  return new Promise((resolve, reject) => {
+    const safeVisitor = visitor.isSafe
+      ? visitor
+      : (...args) => {
+          try {
+            return visitor(...args)
+          } catch (err) {
+            reject(err)
+          }
+          return undefined
         }
-      }
 
-      if (visitResult === false) {
-        // Visitor returned false, indicating a desire to not traverse.
-        resolve()
-      } else if (isPromise(visitResult)) {
-        // We need to execute the result and pass it's result through to our
-        // continuer.
-        visitResult
-          .then(promiseResult => {
-            if (promiseResult === false) {
-              resolve()
+    const recursive = (currentElement, currentContext) =>
+      new Promise(innerResolve => {
+        const doVisit = (getChildren, visitResult, childContext) => {
+          const visitChildren = () => {
+            const child = ensureChild(getChildren())
+            const theChildContext =
+              typeof childContext === 'function' ? childContext() : childContext
+
+            if (child == null) {
+              // If no children then we can't traverse.  We've reached the leaf.
+              innerResolve()
+            } else if (Children.count(child)) {
+              // If its a react Children collection we need to breadth-first
+              // traverse each of them.
+              const mapper = aChild =>
+                aChild ? recursive(aChild, theChildContext) : undefined
+              const children = Children.map(child, cur => cur)
+              // pMapSeries allows us to do depth-first traversal. Thanks @sindresorhus!
+              pMapSeries(children, mapper)
+                .then(innerResolve, reject)
+                .catch(reject)
             } else {
-              visitChildren()
+              // Otherwise we pass the individual child to the next recursion.
+              recursive(child, theChildContext)
+                .then(innerResolve, reject)
+                .catch(reject)
             }
-          })
-          .catch(e => {
-            console.log(
-              'Error occurred in Promise based visitor provided to react-tree-walker.',
-            )
-            if (e) {
-              console.log(e)
-              if (e.stack) {
-                console.log(e.stack)
+          }
+
+          if (visitResult === false) {
+            // Visitor returned false, indicating a desire to not traverse.
+            innerResolve()
+          } else if (isPromise(visitResult)) {
+            // We need to execute the result and pass it's result through to our
+            // continuer.
+            visitResult
+              .then(promiseResult => {
+                if (promiseResult === false) {
+                  innerResolve()
+                } else {
+                  return visitChildren()
+                }
+              })
+              .catch(reject)
+          } else {
+            // Visitor returned true, indicating a desire to continue traversing.
+            visitChildren()
+          }
+        }
+
+        // Is this element a Component?
+        if (typeof currentElement.type === 'function') {
+          const Component = currentElement.type
+          const props = Object.assign(
+            {},
+            Component.defaultProps,
+            currentElement.props,
+          )
+
+          // Is this a class component? (http://bit.ly/2j9Ifk3)
+          const isReactClassComponent =
+            Component.prototype &&
+            (Component.prototype.isReactComponent ||
+              Component.prototype.isPureReactComponent)
+
+          if (isReactClassComponent) {
+            // React class component
+
+            const instance = new Component(props, currentContext)
+
+            // In case the user doesn't pass these to super in the constructor
+            instance.props = instance.props || props
+            instance.context = instance.context || currentContext
+
+            // Make the setState synchronous.
+            instance.setState = newState => {
+              if (typeof newState === 'function') {
+                // eslint-disable-next-line no-param-reassign
+                newState = newState(
+                  instance.state,
+                  instance.props,
+                  instance.context,
+                )
               }
+              instance.state = Object.assign({}, instance.state, newState)
             }
-          })
-      } else {
-        // Visitor returned true, indicating a desire to continue traversing.
-        visitChildren()
-      }
-    }
 
-    // Is this element a Component?
-    if (typeof element.type === 'function') {
-      const Component = element.type
-      const props = Object.assign({}, Component.defaultProps, element.props)
+            doVisit(
+              () => {
+                // Call componentWillMount if it exists.
+                if (instance.componentWillMount) {
+                  instance.componentWillMount()
+                }
 
-      // Is this a class component? (http://bit.ly/2j9Ifk3)
-      const isReactClassComponent =
-        Component.prototype &&
-        (Component.prototype.isReactComponent ||
-          Component.prototype.isPureReactComponent)
+                const children = instance.render()
 
-      if (isReactClassComponent) {
-        // React class component
+                if (
+                  options.componentWillUnmount &&
+                  instance.componentWillUnmount
+                ) {
+                  try {
+                    instance.componentWillUnmount()
+                  } catch (err) {
+                    // This is an experimental feature, we don't want to break
+                    // the bootstrapping process, but lets warn the user it
+                    // occurred.
+                    console.warn(
+                      'Error calling componentWillUnmount whilst walking your react tree',
+                    )
+                    console.warn(err)
+                  }
+                }
 
-        const instance = new Component(props, context)
-
-        // In case the user doesn't pass these to super in the constructor
-        instance.props = instance.props || props
-        instance.context = instance.context || context
-
-        // Make the setState synchronous.
-        instance.setState = newState => {
-          if (typeof newState === 'function') {
-            // eslint-disable-next-line no-param-reassign
-            newState = newState(
-              instance.state,
-              instance.props,
-              instance.context,
+                return children
+              },
+              safeVisitor(currentElement, instance, currentContext),
+              () =>
+                // Ensure the child context is initialised if it is available. We will
+                // need to pass it down the tree.
+                instance.getChildContext
+                  ? Object.assign(
+                      {},
+                      currentContext,
+                      instance.getChildContext(),
+                    )
+                  : currentContext,
+            )
+          } else {
+            // Stateless Functional Component
+            doVisit(
+              () => Component(props, currentContext),
+              safeVisitor(currentElement, null, currentContext),
+              currentContext,
             )
           }
-          instance.state = Object.assign({}, instance.state, newState)
+        } else {
+          // This must be a basic element, such as a string or dom node.
+          doVisit(
+            () =>
+              currentElement.props && currentElement.props.children
+                ? currentElement.props.children
+                : undefined,
+            safeVisitor(currentElement, null, currentContext),
+            currentContext,
+          )
         }
+      })
 
-        doVisit(
-          () => {
-            // Call componentWillMount if it exists.
-            if (instance.componentWillMount) {
-              instance.componentWillMount()
-            }
-
-            const children = instance.render()
-
-            if (options.componentWillUnmount && instance.componentWillUnmount) {
-              try {
-                instance.componentWillUnmount()
-              } catch (err) {
-                // This is an experimental feature, we don't want to break
-                // the bootstrapping process, but lets warn the user it
-                // occurred.
-                console.warn(
-                  'Error calling componentWillUnmount whilst walking your react tree',
-                )
-                console.warn(err)
-              }
-            }
-
-            return children
-          },
-          visitor(element, instance, context),
-          () =>
-            // Ensure the child context is initialised if it is available. We will
-            // need to pass it down the tree.
-            instance.getChildContext
-              ? Object.assign({}, context, instance.getChildContext())
-              : context,
-        )
-      } else {
-        // Stateless Functional Component
-        doVisit(
-          () => Component(props, context),
-          visitor(element, null, context),
-          context,
-        )
-      }
-    } else {
-      // This must be a basic element, such as a string or dom node.
-      doVisit(
-        () =>
-          element.props && element.props.children
-            ? element.props.children
-            : undefined,
-        visitor(element, null, context),
-        context,
-      )
-    }
-  }).catch(err => {
-    // We don't want errors to be swallowed!
-    console.error('Error walking your react tree')
-    console.error(err)
+    recursive(rootElement, rootContext).then(resolve, reject)
   })
 }
